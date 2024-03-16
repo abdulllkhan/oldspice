1772b5341558538919858159087a89a3eff9819c
abdulllkhan <abdulskhan47@gmail.com>
2024-03-16 03:29:14 -0400

    Base setup

---
+++ src/main/java/com/tutofox/oldspice/OldspiceApplication.java
@@ -0,0 +1,13 @@
+package com.tutofox.oldspice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldspiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldspiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutofox/oldspice/OldspiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutofox.oldspice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldspiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
